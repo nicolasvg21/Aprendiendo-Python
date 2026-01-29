c3e012568e86ed0e1a210357f23cba7a9dc68297
Nicolas <nclsvargasgamboa@outlook.es>
2026-01-28 23:35:39 -0500

    va quedando fino

---
+++ Santander academy/4/4.3 conjuntos (set).py
@@ -0,0 +1,3 @@
+print("4.3. CONJUNTOS(SET)")
+print("Un conjunto es una estructura de datos mutable u no ordenada que permite almacenar una coleccion de elementos unicos. Los conjuntos se encierran entre llaves {} o se crean utilizando la funcion set()")
+
