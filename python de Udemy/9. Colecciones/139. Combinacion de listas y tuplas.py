732557bb9ae4cdf2d88395f89e74e692c7587518
Nicolas <nclsvargasgamboa@outlook.es>
2026-02-19 21:58:56 -0500

    19/02/2026

---
+++ python de Udemy/9. Colecciones/139. Combinacion de listas y tuplas.py
@@ -0,0 +1,19 @@
+print("**** COMBINACION D ELISTAS CON TUPLAS ****")
+
+# definir una lista que alcancemos tuplas de productos
+productos = [
+    ("P001", "Camiseta", 20.00),
+    ("P002", "Jeans", 30.00),
+    ("P003", "Sudadera", 40.00),
+]
+
+# Imprimir la informacion de cada producto y ademas calculamos el precio total
+precio_total = 0
+
+print("\nInformacion de los productos: ")
+for producto in productos:
+    # print(producto)
+    id, descripcion, precio = producto # unpacking
+    print(f"Producto: id = {id}, descripcion = {descripcion}, precio = {precio} ")
+    precio_total += precio # producto[2]
+print(f"\nPrecio total de los prodcutos: ${precio_total}")
